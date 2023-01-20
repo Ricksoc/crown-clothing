e5a1798f5a732e89ab6ef41530b9089018ab65e4
ricky <ricky.chaggar@gmail.com>
2023-01-20 12:07:29 +0000

    Convert FormInput to styled component

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,17 +1,15 @@
-import "./FormInput.scss";
+import { FormInputLabel, Input, Group } from "./FormInput.styles.jsx";
 
 const FormInput = ({ label, inputProps }) => {
   return (
-    <div className="group">
-      <input className="form-input" {...inputProps} />
+    <Group>
+      <Input {...inputProps} />
       {label && (
-        <label
-          className={`${inputProps.value.length && "shrink"} form-input-label`}
-        >
+        <FormInputLabel shrink={inputProps.value.length}>
           {label}
-        </label>
+        </FormInputLabel>
       )}
-    </div>
+    </Group>
   );
 };
 
